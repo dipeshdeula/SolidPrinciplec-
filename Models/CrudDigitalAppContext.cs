116fcd529f843d809398c99b105bf72a617e36be
Dipesh Deula <deuladipesh94@gmail.com>
2024-07-19 09:43:14 +0545

    "Database connection with scaffold"

---
+++ Models/CrudDigitalAppContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalDataStructure.Models;
+
+public partial class CrudDigitalAppContext : DbContext
+{
+    public CrudDigitalAppContext()
+    {
+    }
+
+    public CrudDigitalAppContext(DbContextOptions<CrudDigitalAppContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserList> UserLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=Conn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserList>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__UserList__1788CC4C506CB043");
+
+            entity.ToTable("UserList");
+
+            entity.Property(e => e.UserId).ValueGeneratedNever();
+            entity.Property(e => e.EmailAddress).HasMaxLength(50);
+            entity.Property(e => e.UserAddress).HasMaxLength(50);
+            entity.Property(e => e.UserName).HasMaxLength(40);
+            entity.Property(e => e.UserRole).HasMaxLength(50);
+            entity.Property(e => e.UserStatus).HasDefaultValue(true);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/UserList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitalDataStructure.Models;
+
+public partial class UserList
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+
+    public string EmailAddress { get; set; } = null!;
+
+    public string UserProfile { get; set; } = null!;
+
+    public string UserAddress { get; set; } = null!;
+
+    public string UserRole { get; set; } = null!;
+
+    public bool UserStatus { get; set; }
+}
